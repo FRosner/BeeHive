400dd5a5319887f260c9d6d7d885846fe5603a16
FRosner <frank.rosner@student.uni-halle.de>
2013-06-03 21:05:56 +0200

    reduced listener interface

--- src/de/unihalle/sim/main/EventListener.java
+++ src/de/unihalle/sim/main/EventListener.java
@@ -1,12 +1,9 @@
 package de.unihalle.sim.main;
 
 import de.unihalle.sim.entities.PositionedEntity;
-import de.unihalle.sim.util.Position;
 
 public interface EventListener {
 
-	public void notifyPosition(PositionedEntity e);
-
-	public void notifyMovement(PositionedEntity e, Position nextPosition);
+	public void notify(PositionedEntity e);
 
 }
