5d11ef3b4ab71693824b1a64fbc6fe91ba8afe42
FRosner <frank.rosner@student.uni-halle.de>
2013-06-04 20:39:19 +0200

    added simple PrintStreamEventListener

---
+++ src/de/unihalle/sim/main/PrintStreamEventListener.java
@@ -0,0 +1,20 @@
+package de.unihalle.sim.main;
+
+import java.io.PrintStream;
+
+import de.unihalle.sim.entities.PositionedEntity;
+
+public class PrintStreamEventListener implements EventListener {
+
+	PrintStream _out;
+
+	public PrintStreamEventListener(PrintStream out) {
+		_out = out;
+	}
+
+	@Override
+	public void notify(PositionedEntity e) {
+		_out.println(e.getTimeNow() + " " + e.getName() + " " + e.getPosition());
+	}
+
+}
