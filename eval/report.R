ae5ba7a46da75a3bd4143f2e3f8e9a60b40fe876
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-06-15 13:30:38 +0200

    * made plot width flexible * fixed y axis limits to (0,1) for ratio plots

--- eval/report.R
+++ eval/report.R
@@ -1,37 +1,38 @@
 rm(list=ls())
 
-sparsePlot = function(x, y, granularity=100, secondary=FALSE, col="black", title="Bee Plot") {
+sparsePlot = function(x, y, granularity=100, secondary=FALSE, col="black", title="Bee Plot", ylim=c(0,1)) {
   if (length(x) != length(y)) {
     warning("x and y do not have the same length")
   }
   step = ceiling(length(x)/granularity)
   if (secondary) {
     par(new=TRUE)
-    plot(x[c(seq(1,length(x),step),length(x))], y[c(seq(1,length(y),step), length(y))], type="l", axes=FALSE, ylab="", xlab="", bty="c", col=col, main=title)
+    plot(x[c(seq(1,length(x),step),length(x))], y[c(seq(1,length(y),step), length(y))], type="l", axes=FALSE, ylab="", xlab="", bty="c", col=col, main=title, ylim=ylim)
     axis(4)
     mtext("secondary y-axis label",4) 
   } else {
-    plot(x[c(seq(1,length(x),step),length(x))], y[c(seq(1,length(y),step), length(y))], type="l", col=col, main=title)
+    plot(x[c(seq(1,length(x),step),length(x))], y[c(seq(1,length(y),step), length(y))], type="l", col=col, main=title, ylim=ylim)
   }
 }
 
 states = read.csv2("report.csv", dec=".")
 states$time = states$time/3600
 
-granularity=100
+granularity=50
+width=13
 title="Bee Infection Ratio"
-pdf("report_beeInfectionRatio.pdf", title=title)
+pdf("report_beeInfectionRatio.pdf", title=title, width=width)
 sparsePlot(x=states$time, y=states$beeInfectionRatio, granularity=granularity, title=title)
 dev.off()
 title="Number of Bees"
-pdf("report_numberOfBees.pdf", title=title)
-sparsePlot(x=states$time, y=states$numberOfBees, granularity=granularity, title=title)
+pdf("report_numberOfBees.pdf", title=title, width=width)
+sparsePlot(x=states$time, y=states$numberOfBees, granularity=granularity, title=title, ylim=c(0,max(states$numberOfBees)))
 dev.off()
 title="Nectar available (%)"
-pdf("report_averageFlowerNectarRatio.pdf", title=title)
+pdf("report_averageFlowerNectarRatio.pdf", title=title, width=width)
 sparsePlot(x=states$time, y=states$averageFlowerNectarRatio, granularity=granularity, title=title)
 dev.off()
 title="Collapsed Hives (%)"
-pdf("report_collapsedHiveRatio.pdf", title=title)
+pdf("report_collapsedHiveRatio.pdf", title=title, width=width)
 sparsePlot(x=states$time, y=states$collapsedHiveRatio, granularity=granularity, title=title)
 dev.off()
